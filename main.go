ae878674b1ae83c23e8a7f43eec4cab619d602e1
Dan Kleiman <info@dankleiman.com>
2018-01-24 23:22:12 -0500

    initial commit

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	// take a project name as an arg
+	n := flag.String("notepath", notePath(), "Main notebook directory set by env var NOTEPATH")
+	s := flag.String("section", "new_section", "Name for new section of this notebook")
+	flag.Parse()
+	// create a directory with the name flag
+	r := strings.Join([]string{*n, *s}, "/")
+	err := os.MkdirAll(r, 0755)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("Creating new section: %v\n", r)
+	// create a README.md file with h1 of notebook name
+	p := strings.Join([]string{r, "README.md"}, "/")
+	f, err := os.Create(p)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer f.Close()
+	h := strings.Join([]string{"#", r}, " ")
+	f.WriteString(h)
+	fmt.Println("Creating README")
+	// create subdirectories for data, scripts, notes
+	subDirs := []string{"data", "scripts", "notes"}
+	for _, d := range subDirs {
+		dirPath := strings.Join([]string{r, d}, "/")
+		err := os.Mkdir(dirPath, 0755)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Printf("Creating %v section\n", d)
+	}
+}
+
+func notePath() string {
+	if os.Getenv("NOTEPATH") != "" {
+		return os.Getenv("NOTEPATH")
+	}
+	return "notebook"
+}
