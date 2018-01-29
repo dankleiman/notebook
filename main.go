9ecd8a4db64300aebe9074d9ed5d04bcdf07d294
Dan Kleiman <info@dankleiman.com>
2018-01-28 22:15:08 -0500

    clean up messages

--- main.go
+++ main.go
@@ -12,14 +12,17 @@ func main() {
 	n := flag.String("notepath", notePath(), "Main notebook directory set by env var NOTEPATH")
 	s := flag.String("section", "new_section", "Name for new section of this notebook")
 	flag.Parse()
+
 	// create a directory with the name flag
 	r := strings.Join([]string{*n, *s}, "/")
+	fmt.Printf("Creating new section: %v\n", r)
 	err := os.MkdirAll(r, 0755)
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Printf("Creating new section: %v\n", r)
+
 	// create a README.md file with h1 of notebook name
+	fmt.Println("Creating README")
 	p := strings.Join([]string{r, "README.md"}, "/")
 	f, err := os.Create(p)
 	if err != nil {
@@ -28,7 +31,7 @@ func main() {
 	defer f.Close()
 	h := strings.Join([]string{"#", r}, " ")
 	f.WriteString(h)
-	fmt.Println("Creating README")
+
 	// create subdirectories for data, scripts, notes
 	subDirs := []string{"data", "scripts", "notes"}
 	for _, d := range subDirs {
